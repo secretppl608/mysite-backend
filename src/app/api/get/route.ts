eb367b06c7232c5828dbaccf231f8640d68abff0
secretppl608 <lacak3736@gmail.com>
2026-02-25 11:19:31 +0800

    F

--- src/app/api/get/route.ts
+++ src/app/api/get/route.ts
@@ -2,6 +2,5 @@ import { NextRequest, NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 export async function POST(request: NextRequest) {
     const supabase = createClient(process.env.URL!, process.env.PSW!);
-    
-    
+    // get 接口，正在施工
 }
--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -1,15 +1,39 @@
 import { NextRequest, NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
+import { Client, Pool } from "pg";
 export async function POST(request: NextRequest) {
-    const supabase = createClient(process.env.URL!, process.env.PSW!);
+    const pool = new Client({
+        host: "db.zmzbyripoxydefleryhn.supabase.co",
+        port: 6543,
+        database: "postgres",
+        user: "postgres.zmzbyripoxydefleryhn",
+        password: process.env.DB_PSW,
+        connectionTimeoutMillis: 5000,
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    });
     const d = await request.json();
-    if (d.title && d.content){
-        const {data,error} =  await supabase.from("post").insert([
-            {id:Date.now(),title:d.title,content:d.content}
-        ]);
-        if (error) return NextResponse.json({reason:"we_dont_know_what_the_hell_because_database_is_crazy"},{status:500})
-        return NextResponse.json({reason:"OK"},{status:200})
+    if (d.title && d.content) {
+        await pool.connect();
+        try {
+            await pool.query("INSERT INTO post (id,title,content) VALUES ($1,$2,$3)",[Date.now(),d.title,d.content]);
+        } catch (error) {
+            pool.end();
+            return NextResponse.json(
+                {
+                    reason: "we_dont_know_what_the_hell_because_database_is_crazy",
+                },
+                { status: 500 },
+            );
+        }
+        pool.end();
+        return NextResponse.json({ reason: "OK" }, { status: 200 });
     } else {
-        return NextResponse.json({reason:"no_title_or_content"},{status:400})
+        pool.end();
+        return NextResponse.json(
+            { reason: "no_title_or_content" },
+            { status: 400 },
+        );
     }
 }
